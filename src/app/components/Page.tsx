7cb9cf24f736d8c857c4d818a5e2c039a5ad3d7e
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-04-27 07:54:36 -0500

    feat: add observer to page component

--- src/app/components/Page.tsx
+++ src/app/components/Page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useEffect, useRef, useState } from 'react';
 import { themes, useThemeContext } from '../theme';
 import { DocSection, DocsStructure } from '../types/types';
 import Main from './Main';
@@ -16,11 +17,41 @@ export default function Page({
 }) {
   const { theme } = useThemeContext();
   const themeStyles = themes[theme];
+  const docRef = useRef<HTMLDivElement>(null);
+  const [currentSection, setCurrentSection] = useState('');
+
+  useEffect(() => {
+    const titles =
+      document.querySelectorAll<HTMLHeadingElement>(
+        '.section-title'
+      );
+
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            setCurrentSection(entry.target.id);
+          }
+        });
+      },
+      {
+        root: null,
+        rootMargin: '-10% 0% -80% 0%',
+        threshold: 1,
+      }
+    );
+
+    titles.forEach(title => observer.observe(title));
+
+    return () => {
+      titles.forEach(title => observer.unobserve(title));
+    };
+  }, []);
 
   return (
     <div className={styles.wrapper} style={themeStyles}>
       <SideBar version={version} />
-      <div className={styles.container}>
+      <div ref={docRef} className={styles.container}>
         <NavBar version={version} />
         <Main versionSections={versionSections} />
       </div>
